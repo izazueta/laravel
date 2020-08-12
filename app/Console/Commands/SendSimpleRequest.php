e3e9a331668b8ca3c1c2ad93a5c7958946346d8a
Ivan Zazueta <ivan@healthlabs.com>
2020-08-11 22:11:00 -0500

    Update command to send a simple POST request

--- app/Console/Commands/SendSimpleRequest.php
+++ app/Console/Commands/SendSimpleRequest.php
@@ -1,8 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
 
 class SendSimpleRequest extends Command
 {
@@ -11,32 +14,30 @@ class SendSimpleRequest extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'incfile:send-simple-request';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    protected $description = 'Send a simple POST request';
 
     /**
      * Execute the console command.
      *
      * @return int
      */
-    public function handle()
+    public function handle(): int
     {
+        $url = 'https://atomic.incfile.com/fakepost';
+
+        $this->info("Making a simple post request to $url");
+
+        $response = Http::post($url);
+
+        $this->info("Status: {$response->status()}");
+
         return 0;
     }
 }
